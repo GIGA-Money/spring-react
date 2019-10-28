f7f4174ed2b5ca62199cddc77b3f16d2b3c01d08
GIGA-Money <christopherregan@outlook.com>
2019-10-28 12:23:48 -0400

    work on the car entity

--- src/main/java/com/springreact/springreact/SpringreactApplication.java
+++ src/main/java/com/springreact/springreact/SpringreactApplication.java
@@ -1,13 +1,20 @@
 package com.springreact.springreact;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class SpringreactApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpringreactApplication.class);
+  
 	public static void main(String[] args) {
 		SpringApplication.run(SpringreactApplication.class, args);
+		logger.info("anything goes");
 	}
 
 }
---
+++ src/main/java/com/springreact/springreact/domain/CarEntity.java
@@ -0,0 +1,18 @@
+package com.springreact.springreact.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+/**
+ * 
+ * @author GIGA-Money
+ * This is the car entity class.
+ *
+ */
+public class CarEntity {
+  
+  
+}
