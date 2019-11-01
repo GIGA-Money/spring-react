735040ecf58005b5da0de550ddda3083ba5ca91c
Christopher Michael Regan <gigachris1@hotmail.com>
2019-10-31 22:45:23 -0400

    added car controller

---
+++ src/main/java/com/springreact/springreact/web/CarController.java
@@ -0,0 +1,19 @@
+package com.springreact.springreact.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.springreact.springreact.domain.CarEntity;
+import com.springreact.springreact.domain.CarRepo;
+
+@RestController
+public class CarController {
+
+  @Autowired
+  private CarRepo localRepo;
+
+  @GetMapping("/cars")
+  public Iterable<CarEntity> getCars() {
+    return localRepo.findAll();
+  }
+}
