ed3ad955df7eeaf63162a66f98e894c46f6d3340
Christopher Michael Regan <gigachris1@hotmail.com>
2019-11-04 15:31:21 -0500

    updated owner entity to address comments

--- src/main/java/com/springreact/springreact/domain/OwnerEntity.java
+++ src/main/java/com/springreact/springreact/domain/OwnerEntity.java
@@ -12,10 +12,14 @@ import lombok.Data;
 @Entity
 @Data
 public class OwnerEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long owerid;
+  private String firstname, lastname;
   /**
-   * cascade defines how cascading affects the entities. the attribute all means that if the owner is
-   * deleted the cars linked to that owner are deleted as well. the mapped by owner,, tesll us that
-   * the car class has the owner field, which is the foreign key for this relationship.
+   * cascade defines how cascading affects the entities. the attribute all means that if the owner
+   * is deleted the cars linked to that owner are deleted as well. the mapped by owner,, tesll us
+   * that the car class has the owner field, which is the foreign key for this relationship.
    */
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private List<CarEntity> cars;
@@ -31,10 +35,7 @@ public class OwnerEntity {
     this.cars = cars;
   }
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long owerid;
-  private String firstname, lastname;
+  public OwnerEntity() {}
 
   public OwnerEntity(String firstname, String lastname) {
     super();
